668cca1ed88ead968f27f26aaa851f1a5b9f8fb1
Ademir Goulart <ademir.goulart@gmail.com>
2022-01-07 17:15:32 -0300

    consulta simples com query  methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post,String>{
+public interface PostRepository extends MongoRepository<Post, String> {
 
-
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
